999da2f91c22bf0d33ef6a96488c28e861957f5d
nikawadori <nikawa.2161.t@icloud.com>
2020-12-03 22:31:46 +0900

    validatesの設定

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,12 @@
 class Article < ApplicationRecord
+  validates :title, presence: true
+  validates :body, presence: true
+  validates :Job_title, presence: true
+  validates :employment, presence: true
+  validates :education, presence: true
+  validates :licensure, presence: true
+  validates :documents, presence: true
+  validates :selection, presence: true
+  validates :screening_location, presence: true
+
 end
