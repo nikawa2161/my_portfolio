111ca1056ff19a4cbb63d7bb9db5b30554369a5f
nikawadori <nikawa.2161.t@gmail.com>
2020-12-04 16:57:40 +0900

    rollbackでのDB編集

--- db/migrate/20201204064014_add_company_id_to_articles.rb
+++
@@ -1,5 +0,0 @@
-class AddCompanyIdToArticles < ActiveRecord::Migration[6.0]
-  def change
-    add_reference :articles, :company
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_04_064014) do
+ActiveRecord::Schema.define(version: 2020_12_02_133514) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "articles", force: :cascade do |t|
+    t.bigint "company_id", null: false
     t.string "title", null: false
     t.text "body", null: false
     t.string "Job_title", null: false
@@ -27,7 +28,6 @@ ActiveRecord::Schema.define(version: 2020_12_04_064014) do
     t.string "screening_location", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.bigint "company_id"
     t.index ["company_id"], name: "index_articles_on_company_id"
   end
 
