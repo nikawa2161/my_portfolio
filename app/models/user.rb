0e956a224ca247d0ea829b0625cd649edeba2043
nikawadori <nikawa.2161.t@gmail.com>
2020-12-06 23:25:32 +0900

    likeモデルの作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+   has_many :likes, dependent: :destroy
    has_one :profile, dependent: :destroy
 
    delegate :birthday, :age, :gender, to: :profile, allow_nil: true
