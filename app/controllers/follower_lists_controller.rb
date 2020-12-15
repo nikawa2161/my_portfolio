5d8eaf7dc56e7d4cfd3138c2179bc7bd2cdf51bd
nikawadori <nikawa.2161.t@gmail.com>
2020-12-15 19:32:20 +0900

    フォロワーの一覧ページ　未解決

--- app/controllers/follower_lists_controller.rb
+++ app/controllers/follower_lists_controller.rb
@@ -2,6 +2,8 @@ class FollowerListsController < ApplicationController
   before_action :authenticate_company!
   
   def show
+    user_ids = current_company.followers.pluck(:id)
+    @profile = Profile.where(user_id: user_ids)
   end
 
 end
