b479e816309d29f952f029e4b31ca2912c630a33
nikawadori <nikawa.2161.t@gmail.com>
2020-12-15 16:27:06 +0900

    フォロー一覧ページに特定の記事表示

--- app/controllers/following_lists_controller.rb
+++ app/controllers/following_lists_controller.rb
@@ -1,4 +1,9 @@
 class FollowingListsController < ApplicationController
+  before_action :authenticate_user!
+  
   def show
+    company_ids = current_user.followings.pluck(:id)
+    @articles = Article.where(company_id: company_ids)
   end
+
 end
