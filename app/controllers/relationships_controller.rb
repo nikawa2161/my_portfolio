9abe10d6581ccc0405f0552ae6bec52826d1458b
nikawadori <nikawa.2161.t@gmail.com>
2020-12-15 14:22:20 +0900

    プロフィールページの修正とcommonファイルを作成し共通化。

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,5 +1,8 @@
 class RelationshipsController < ApplicationController
   def show
     @company = Company.find(params[:id])
+    if @company == current_company
+      redirect_to company_profile_path
+    end
   end
 end
