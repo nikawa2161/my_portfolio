d00b7519522c9b4bda505c13cc8036565f6d7d95
nikawadori <nikawa.2161.t@gmail.com>
2021-01-11 17:11:06 +0900

    ユーザー簡単登録実装

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,7 @@ Rails.application.routes.draw do
   }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'top#index'
+  post '/top/guest_sign_in', to: 'top#new_guest'
   resources :articles do
     resource :like, only: [:show, :create, :destroy]
   end
