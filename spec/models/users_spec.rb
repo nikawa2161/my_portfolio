2293b2bb443d268f4c49eacf8bf71358b42197e1
nikawadori <nikawa.2161.t@gmail.com>
2021-01-14 09:40:59 +0900

    ユーザーモデルの記述2

---
+++ spec/models/users_spec.rb
@@ -0,0 +1,32 @@
+require 'rails_helper'
+
+RSpec.describe 'Users', type: :request do
+
+  context 'バリデーション' do
+
+    it '姓、名、メール、パスワードがある場合、有効である' do
+      user = FactoryBot.build(:user)
+      expect(user).to be_valid
+    end
+
+    it 'メールアドレスがない場合、無効である'  do
+      user = FactoryBot.build(:user, email: nil)
+      user.valid?
+      expect(user.errors[:email]).to include('を入力してください')
+    end
+
+    it '重複したメールアドレスの場合、無効である' do
+      user1 = FactoryBot.create(:user, email: 'testman@example.com')
+      user2 = FactoryBot.build(:user, email: 'testman@example.com')
+      user2.valid?
+      expect(user2.errors[:email]).to include('はすでに存在します')
+    end
+
+    it 'パスワードがない場合、無効である' do
+      user = FactoryBot.build(:user, password: nil)
+      user.valid?
+      expect(user.errors[:password]).to include('を入力してください')
+    end
+
+  end
+end
